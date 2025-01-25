1cd50c188d2778de87499a5240cf128a33f27443
Wangzy <wangziyuan455@Gmail.com>
2025-01-25 18:38:41 +0800

    feat: 后端返回结果的封装类

---
+++ tiktok-cart/src/main/java/com/example/tiktokcart/controller/ShopCartController.java
@@ -0,0 +1,13 @@
+package com.example.tiktokcart.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 购物车服务相关接口
+ */
+@Slf4j
+@RestController
+public class ShopCartController {
+
+}
