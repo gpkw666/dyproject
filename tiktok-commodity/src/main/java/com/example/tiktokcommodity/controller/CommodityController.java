9db6394d975a2bbe0a37a8b43c7b416be3917a24
Wangzy <wangziyuan455@Gmail.com>
2025-01-25 19:56:25 +0800

    feat: 库表设计

---
+++ tiktok-commodity/src/main/java/com/example/tiktokcommodity/controller/CommodityController.java
@@ -0,0 +1,14 @@
+package com.example.tiktokcommodity.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 商品服务相关接口
+ */
+@Slf4j
+@RestController
+public class CommodityController {
+
+}
