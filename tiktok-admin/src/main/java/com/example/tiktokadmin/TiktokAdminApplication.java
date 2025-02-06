b6103ff81a95186be8463f66b05b4940854eb315
Wangzy <wangziyuan455@Gmail.com>
2025-02-06 16:09:10 +0800

    feat: 添加后台管理模块

---
+++ tiktok-admin/src/main/java/com/example/tiktokadmin/TiktokAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiktokadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiktokAdminApplication.class, args);
+    }
+
+}
---
+++ tiktok-admin/src/test/java/com/example/tiktokadmin/TiktokAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiktokadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
